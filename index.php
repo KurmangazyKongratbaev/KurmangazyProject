19c3d4fec9c2bbe8ae1163438c11a0732a4a88aa
Kurmangazy <cvbn_999@bk.ru>
2016-05-23 11:54:13 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,216 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+      
+    <title>Welcome</title>
+
+    <!-- Bootstrap -->
+    <link href="resources/css/bootstrap.css" rel="stylesheet">
+    <link href="resources/css/style.css" rel="stylesheet">
+      
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,300,700' rel='stylesheet' type='text/css'>
+  </head>
+  <body>
+    <div id="top"></div>
+    <header class="header">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 ">
+                    <div class="navbar-header">
+                            <a type="button" class="navbar-toggle menu-button" data-toggle="collapse" data-target="#myNavbar">
+                                <span class="col-logo glyphicon glyphicon-th"></span>
+                            </a>
+                            <a class="navbar-brand logo" href="index.php">Welcome to Project</a>
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <nav class="collapse navbar-collapse" id="myNavbar" role="navigation">
+                        <ul class="nav navbar-nav navbar-right menu">
+                                <li><a href="services.php">Services</a></li>
+                                <li><a href="about.html">About us</a></li>
+                                <li><a href="#contact">Contacts</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+      
+	<!-- MAIN IMAGE SECTION -->
+	<div class="main-theme" style="background-image: url(resources/images/main-theme.png);">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8 col-lg-offset-2">
+					<h1>BooKing.kz</h1>
+					<h2>Welcome to my project</h2>
+					<h2><a href="services.php" class="btn btn-default butt">Book</a></h2>
+					<div class="downTo"><i class="glyphicon glyphicon-chevron-down"></i></div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	
+    <div class="container next">
+		<div class="row mt">
+			<div class="col-lg-12">
+				<h1>Today's most popular deals</h1>	
+				<br/>	
+		    </div>			
+		</div>
+         <div id="myCarousel" class="carousel slide" data-ride="carousel">
+
+      <!-- Wrapper for slides -->
+      <div class="carousel-inner" role="listbox">
+        <div class="item active">
+          <div class="container">
+            <div class="row">
+                <div class="col-lg-4">
+                    <a href="about.html"><img class="img" src="resources/images/deals/1.jpg" alt="Deal of the day" width="100%"></a>
+                </div>
+                <div class="col-lg-4">
+                    <a href="about.html"><img class="img" src="resources/images/deals/2.jpg" alt="Deal of the day" width="100%"></a>
+                </div>
+                <div class="col-lg-4">
+                    <a href="about.html"><img class="img" src="resources/images/deals/3.jpg" alt="Deal of the day" width="100%"></a>
+                </div>		
+            </div>
+	       </div>
+        </div>
+
+        <div class="item">
+          <div class="container">
+            <div class="row">
+                <div class="col-lg-4">
+                    <a href="about.html"><img class="img" src="resources/images/deals/4.jpg" alt="Deal of the day" width="100%"></a>
+                </div>
+                <div class="col-lg-4">
+                    <a href="about.html"href="about.html"><img class="img" src="resources/images/deals/5.jpg" alt="Deal of the day" width="100%"></a>
+                </div>
+                <div class="col-lg-4">
+                    <a href="about.html"><img class="img" src="resources/images/deals/6.jpg" alt="Deal of the day" width="100%"></a>
+                </div>		
+            </div>
+	       </div>
+        </div>
+        <div class="control-box">                            
+            <a data-slide="prev" href="#myCarousel" class="carousel-control left">‹</a>
+            <a data-slide="next" href="#myCarousel" class="carousel-control right">›</a>
+        </div><!-- /.control-box -->
+      </div>
+    </div>  
+	</div>
+      
+     <!-- SERVICES -->
+    <section id="services">
+    </section>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<h1>Services</h1>
+			</div>
+			<div class="col-lg-8">
+				<p>Our top offers for special people</p>
+			</div>
+			<div class="col-lg-4 goright">
+				<p><a href="services.php">See All Services</a></p>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-lg-8">
+				<img class="img-responsive" src="resources/images/main-res.jpg">
+				<h3>RESTAURANTS</h3>
+				<p>ANDOCA - the best place near to LOCA and the best rating around our services.</p>
+				<p><a href="services.php">Read More</a></p>
+			</div>
+			<div class="col-lg-4">
+				<img class="img-responsive" src="resources/images/cafe.jpg">
+				<h3>CAFES</h3>
+				<p>FREECOFFEE - the best rating in 2015 y. and with free coffee.</p>
+				<p><a href="services.php">Read More</a></p>
+			</div>		
+		</div>
+	</div>
+
+	
+   
+	<div><br></div>
+    <section id="contact"></section>
+	<div class="social">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-4 dg">
+					<h4 class="ml"><i class="glyphicon glyphicon-envelope" style="font-size:15px;"></i>&nbsp&nbsp&nbsp EMAIL</h4>
+                    <a href="about.html"><p class="ml">> Subscribe for our latest offers</p></a>
+				</div>
+				<div class="col-lg-4 lg">
+					<h4 class="ml"><i class="glyphicon glyphicon-phone" style="font-size:15px;"></i>&nbsp&nbsp&nbsp LOTTA APP</h4>
+					<p class="ml">> Download app and book on the go</p>
+				</div>
+				<div class="col-lg-4 dg">
+					<h4 class="ml"><i class="glyphicon glyphicon-globe" style="font-size:15px;"></i>&nbsp&nbsp&nbsp LOTTA REWARDS</h4>
+                    <a href="services.php"><p class="ml">> Book now and save up 40%</p></a>
+				</div>
+			</div>
+		</div>
+	</div>
+      
+	<div class="contact" id="contact">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8">
+                    <p>
+						<a href="services.php">Services</a><br/>
+                        <a href="about.html">About us</a><br/>
+                        <a href="admin">Admin panel</a><br/>
+					</p>
+					<p style="font-size:21px;">
+						Kaskelen city, Ablaykhan 1/1,<br/>
+						Kongratbaev Kurmangazy.
+					</p>
+					<p>
+						Phone: +7 702 631 34 71<br/>
+					</p>
+                    
+				</div>
+				<div class="col-lg-4">
+				</div>
+                <div class="col-lg-12">
+				<p class="centered">&copy;KURMANGAZY</p>
+			</div>
+			</div>
+		</div>
+	</div>
+      
+      
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="resources/js/bootstrap.min.js"></script>
+    <script src="js/jquery.js"></script>
+    <script>
+    // Scrolls to the selected menu item on the page
+    $(function() {
+        $('a[href=#contact]').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+    </script>
+  </body>
+</html>
---
+++ services.php
@@ -0,0 +1,245 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+      
+    <title>Services</title>
+
+    <!-- Bootstrap -->
+    <link href="resources/css/bootstrap.css" rel="stylesheet">
+    <link href="resources/css/style.css" rel="stylesheet">
+      
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,300,700' rel='stylesheet' type='text/css'>
+  </head>
+  <body>
+    <div id="top"></div>
+    <header class="header">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 ">
+                    <div class="navbar-header">
+                            <a type="button" class="navbar-toggle menu-button" data-toggle="collapse" data-target="#myNavbar">
+                                <span class="col-logo glyphicon glyphicon-th"></span>
+                            </a>
+                            <a class="navbar-brand logo" href="index.php">Welcome to Project</a>
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <nav class="collapse navbar-collapse" id="myNavbar" role="navigation">
+                        <ul class="nav navbar-nav navbar-right menu">
+                                <li><a href="about.html">About us</a></li>
+                                <li><a href="#contact">Contacts</a></li>
+                                <li><a href="admin">Admin panel</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+      
+	<!-- MAIN IMAGE SECTION -->
+	<div class="main-theme" style="background-image: url(resources/images/main-theme.png);">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8 col-lg-offset-2">
+					<h1>BooKing.kz</h1>
+					<h2>Welcome to my project</h2>
+					<h2><a href="#services" class="btn btn-default butt">Book now</a></h2>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	
+    <div class="container next">
+		<div class="row mt">
+			<div class="col-lg-12">
+				<h1>Today's most popular deals</h1>	
+				<br/>	
+		    </div>			
+		</div>
+         <div id="myCarousel" class="carousel slide" data-ride="carousel">
+
+      <!-- Wrapper for slides -->
+      <div class="carousel-inner" role="listbox">
+        <div class="item active">
+          <div class="container">
+            <div class="row">
+                <div class="col-lg-4">
+                    <a href="about.html"><img class="img" src="resources/images/deals/1.jpg" alt="Deal of the day" width="100%"></a>
+                </div>
+                <div class="col-lg-4">
+                    <a href="about.html"><img class="img" src="resources/images/deals/2.jpg" alt="Deal of the day" width="100%"></a>
+                </div>
+                <div class="col-lg-4">
+                    <a href="about.html"><img class="img" src="resources/images/deals/3.jpg" alt="Deal of the day" width="100%"></a>
+                </div>		
+            </div>
+	       </div>
+        </div>
+
+        <div class="item">
+          <div class="container">
+            <div class="row">
+                <div class="col-lg-4">
+                    <a href="about.html"><img class="img" src="resources/images/deals/4.jpg" alt="Deal of the day" width="100%"></a>
+                </div>
+                <div class="col-lg-4">
+                    <a href="about.html"href="about.html"><img class="img" src="resources/images/deals/5.jpg" alt="Deal of the day" width="100%"></a>
+                </div>
+                <div class="col-lg-4">
+                    <a href="about.html"><img class="img" src="resources/images/deals/6.jpg" alt="Deal of the day" width="100%"></a>
+                </div>		
+            </div>
+	       </div>
+        </div>
+        <div class="control-box">                            
+            <a data-slide="prev" href="#myCarousel" class="carousel-control left">‹</a>
+            <a data-slide="next" href="#myCarousel" class="carousel-control right">›</a>
+        </div><!-- /.control-box -->
+      </div>
+    </div>  
+	</div>
+      
+     <!-- SERVICES SECTION -->
+    <div id="services">
+        <div class="container">
+            <div class="row mt">
+                <div class="col-lg-12 ser">
+                    <h1>RESTAURANTS</h1>
+                    <br>
+                </div>
+                <div class="col-lg-4">
+                    <img class="img-responsive" src="resources/images/reses/1.jpg">
+                    <h3>Central</h3>
+                    <p>Chef Virgilio Martinez has taken Peruvian cuisine to a whole new extreme elevation. Central, which captured the top spot on Latin America’s 50 Best Restaurants list in 2014, takes diners on a vertical journey across Peru’s landscape, serving native ingredients sourced at various altitudes...</p>
+                    <p><a class="read" href="services/central.php">Read More</a></p>
+                </div>
+
+                <div class="col-lg-4">
+                    <img class="img-responsive" src="resources/images/reses/2.jpg">
+                    <h3>Eleven Madison Park</h3>
+                    <p>If the recipe for an outstanding restaurant is creating the perfect balance between great food and impeccable service, then Eleven Madison Park co-owners Daniel Humm and Will Guidara have hit the jackpot...</p>
+                    <p><a class="read" href="services/elevenmadisonpark.php">Read More</a></p>
+                </div>
+            
+                <div class="col-lg-4">
+                    <img class="img-responsive" src="resources/images/reses/3.jpg">
+                    <h3>Noma</h3>
+                    <p>It has been something of a whirlwind 12 months for René Redzepi and the team from Noma. As well as reclaiming the number one spot in the 2014 edition of The World’s 50 Best Restaurants list, Redzepi upped sticks and moved Noma to Japan for a hugely over-subscribed five-week pop-up earlier this year that received wild critical acclaim...</p>
+                    <p><a class="read" href="services/noma.php">Read More</a></p>
+                </div>            
+            </div>
+        </div>
+    </div>
+
+    <div id="services-cafe">
+        <div class="container">
+            <div class="row mt">
+                <div class="col-lg-12 ser">
+                    <h1>CAFES</h1>
+                    <br>
+                </div>
+                <div class="col-lg-4">
+                    <img class="img-responsive" src="resources/images/cafes/1.jpg">
+                    <h3>Old Cafe Bistio</h3>
+                    <p>There's the famous Recoleta Cemetery, the Casa Rosada, and the landmark Obelisk, but the more recent MALBA museum should also top the list of attractions. It symbolizes the city's new identity...</p>
+                    <p><a class="read" href="services/oldcafebistio.php">Read More</a></p>
+                </div>
+
+                <div class="col-lg-4">
+                    <img class="img-responsive" src="resources/images/cafes/2.jpg">
+                    <h3>Naples</h3>
+                    <p>It is not the fairytale that is Venice, the cosmopolitan center that is Milan, or the museum city that is Rome, but Naples and its surroundings are the very soul of Italy and the perfect escape that provides both urban excitement and sophisticated seclusion...</p>
+                    <p><a class="read" href="services/naples.php">Read More</a></p>
+                </div>
+            
+                <div class="col-lg-4">
+                    <img class="img-responsive" src="resources/images/cafes/3.jpg">
+                    <h3>Bek De Rio</h3>
+                    <p>So pack your Havaianas and your sexiest beachwear, and whether you'll be heading to the beach, to a colorful dance venue, or crossing over to Niteroi's contemporary art museum...</p>
+                    <p><a class="read" href="services/bekderaio.php">Read More</a></p>
+                </div>            
+            </div>
+        </div>
+    </div>
+	
+   
+	<div><br></div>
+    <section id="contact"></section>
+	<div class="social">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-4 dg">
+					<h4 class="ml"><i class="glyphicon glyphicon-envelope" style="font-size:15px;"></i>&nbsp&nbsp&nbsp EMAIL</h4>
+                    <a href="about.html"><p class="ml">> Subscribe for our latest offers</p></a>
+				</div>
+				<div class="col-lg-4 lg">
+					<h4 class="ml"><i class="glyphicon glyphicon-phone" style="font-size:15px;"></i>&nbsp&nbsp&nbsp LOTTA APP</h4>
+					<p class="ml">> Download app and book on the go</p>
+				</div>
+				<div class="col-lg-4 dg">
+					<h4 class="ml"><i class="glyphicon glyphicon-globe" style="font-size:15px;"></i>&nbsp&nbsp&nbsp LOTTA REWARDS</h4>
+                    <a href="services.php"><p class="ml">> Book now and save up 40%</p></a>
+				</div>
+			</div>
+		</div>
+	</div>
+      
+	<div class="contact" id="contact">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8">
+                    <p>
+						<a href="services.php">Services</a><br/>
+                        <a href="about.html">About us</a><br/>
+                        <a href="admin">Admin panel</a><br/>
+					</p>
+					<p style="font-size:21px;">
+						Kaskelen city, Ablaykhan 1/1,<br/>
+						Kongratbaev Kurmangazy.
+					</p>
+					<p>
+						Phone: +7 702 631 34 71<br/>
+					</p>
+                    
+				</div>
+				<div class="col-lg-4">
+				</div>
+                <div class="col-lg-12">
+				<p class="centered">&copy;KURMANGAZY</p>
+			</div>
+			</div>
+		</div>
+	</div>
+      
+      
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="resources/js/bootstrap.min.js"></script>
+        <script src="js/jquery.js"></script>
+    <script>
+    // Scrolls to the selected menu item on the page
+    $(function() {
+        $('a[href=#contact]').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+    </script>
+  </body>
+</html>
